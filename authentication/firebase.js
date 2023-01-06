ee93c3fb6e61cb5ca4225dffcc79007de4b1ec4d
ribhu27 <93272559+ribhu27@users.noreply.github.com>
2023-01-06 14:34:33 +0530

    Add files via upload

---
+++ authentication/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDX4EfePUh5o_4LaphrxS5akusFGVpXMDQ",
+  authDomain: "auth-2-643a3.firebaseapp.com",
+  projectId: "auth-2-643a3",
+  storageBucket: "auth-2-643a3.appspot.com",
+  messagingSenderId: "957786289635",
+  appId: "1:957786289635:web:d7970dd1e0d2f459ee5cd6"
+    
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
